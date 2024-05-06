6eb4ce957060b70dc696ce19c015298d93d49241
tyedusi <gs1sadwsdaw@hotmail.com>
2024-05-06 19:34:42 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const gcd = (a, b) => {
-  if (b === 0) {
-    return a;
+function uniquePathsWithObstacles(obstacleGrid) {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  if (obstacleGrid[0][0] === 1) return 0;
+  dp[0][0] = 1;
+  for (let i = 1; i < m; i++) {
+    if (obstacleGrid[i][0] === 0) dp[i][0] = dp[i - 1][0];
   }
-  return gcd(b, a % b);
-};
+  for (let j = 1; j < n; j++) {
+    if (obstacleGrid[0][j] === 0) dp[0][j] = dp[0][j - 1];
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i][j] === 0) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+}
