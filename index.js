0602a0912ada6fb13cb5fc35bbe63333f4817cf8
tyedusi <gs1sadwsdaw@hotmail.com>
2024-05-15 01:52:26 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const head = (arr) => arr[0];
+const countingSort = (arr) => {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const count = Array(max - min + 1).fill(0);
+  for (let num of arr) {
+    count[num - min]++;
+  }
+  let sortedIndex = 0;
+  for (let i = min; i <= max; i++) {
+    while (count[i - min] > 0) {
+      arr[sortedIndex++] = i;
+      count[i - min]--;
+    }
+  }
+  return arr;
+};
